4fca3f0206b61fc3bd7f48900f3dc0c0c3f9e8be
Khuong19 <133074738+Khuong19@users.noreply.github.com>
2023-11-05 00:52:15 +0700

    api error

--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -1,41 +1,36 @@
-import { useDispatch, useSelector } from 'react-redux';
-import Image from 'next/image';
-import React, { useState } from 'react'
-import { searchResultsSlice } from '../redux/slices/searchResultsSlice';
-
+import { useState } from "react";
 const SearchButton = () => {
-  const dispatch = useDispatch();
-
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleSearch = async () => {
-    const response = await axios.get(`http://5.104.83.78:1337/api/search-law-post?search=${searchTerm}`);
-    const searchResults = response.data;
-
-    dispatch(searchResultsSlice.actions.updateSearchResults(searchResults));
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+  
+    const handleSearch = async () => {
+      const response = await axios.get(`http://5.104.83.78:1337/api/search-law-post?search=${searchTerm}`);
+      const searchResults = response.data;
+  
+      setSearchResults(searchResults);
+    };
+  
+    return (
+      <form className='w-[450px]'>
+        <div className='relative flex flex-wrap items-stretch sm:w-full gap-1'>
+          <input
+            type='text'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            id='default-search'
+            className='relative flex-auto border-gray-300 border-r-0 block min-w-0 rounded-3xl border bg-white dark:bg-[212529] dark:border-gray-500 focus:transition-all focus:ease-in-out focus:z-10 py-[11px] px-[16px] focus:drop-shadow-3xl focus:border-focus-blue focus:border-4 Class Properties outline-none'
+            required
+          />
+          <button
+            onClick={handleSearch}
+            type='submit'
+            className='flex relative z-2 right-0 bottom-0 ml-[-58px] px-4 py-3 border-gray-300 border rounded-3xl bg-white'
+          >
+            <img src='search.svg' alt='' width={20} height={20} />
+          </button>
+        </div>
+      </form>
+    );
   };
-
-  return (
-    <form className='w-[450px]'>
-      <div className='relative flex flex-wrap items-stretch sm:w-full gap-1'>
-        <input
-          type='text'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          id='default-search'
-          className='relative flex-auto border-gray-300 border-r-0 block min-w-0 rounded-3xl border bg-white dark:bg-[212529] dark:border-gray-500 focus:transition-all focus:ease-in-out focus:z-10 py-[11px] px-[16px] focus:drop-shadow-3xl focus:border-focus-blue focus:border-4 Class Properties outline-none'
-          required
-        />
-        <button
-          onClick={handleSearch}
-          type='submit'
-          className='flex relative z-2 right-0 bottom-0 ml-[-58px] px-4 py-3 border-gray-300 border rounded-3xl bg-white'
-        >
-          <Image src='search.svg' alt='' width={20} height={20} />
-        </button>
-      </div>
-    </form>
-  );
-};
-
-export default SearchButton;
+  
+  export default SearchButton;
--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -6,23 +6,29 @@ import Header from '../components/Header'
 import { Pagination } from 'antd'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useSelector } from 'react-redux';
-const Result = () => {
-  const searchResults = useSelector((state) => state.searchResults);
-  const [data, setData] = useState([]);
 
+const Result = () => {
   const router = useRouter();
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    if (router.query && router.query.searchTerm) {
+      const { searchTerm } = router.query;
+      setSearchTerm(searchTerm);
+    }
+  }, [router.query]);
 
   const fetchData = async () => {
     const response = await axios.get(
-      `http://5.104.83.78:1337/api/search-law-post?search=48/2023/NQ-HĐND&page=${currentPage}`
+      `http://5.104.83.78:1337/api/search-law-post?search=${searchTerm}&page=${currentPage}`
     );
     setData(response.data);
   };
 
   useEffect(() => {
     fetchData();
-  }, [currentPage]);
+  }, [searchTerm, currentPage]);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -92,61 +98,7 @@ const Result = () => {
         </div>
 
       )}
-      {/* <div className='al py-2 bg-light-blue flex flex-col gap-4 px-9 my-[16px] dark:bg-[#032830]'>
-          <h2 className='text-xl'>
-            <small className='text-[15px] badge p-1 rounded-md mr-2 bg-[#0dcaf0] text-black py-[3px] px-[9.25px]'>AL</small>
-            <Link href='/resultAl' className='text-[#0A58CA] dark:text-[#6ea8fe]'>
-            Về xem xét quyết định hành chính có liên quan đến hành vi hành chính bị khiếu kiện
-            </Link>
-          </h2>
-
-          <nav className='nav mb-2 flex items-center gap-5 justify-start text-sm dark:text-light-gray'>
-            <span className='nav-item'>
-              <span className='font-bold'>Mã: </span>
-              63/2023/AL
-            </span>
-            <span className='nav-item'>
-              <span className='font-bold'>Ngày thông qua: </span>
-              03/08/2022
-            </span>
-            <span className='nav-item'>
-              <span className='font-bold'>Hiệu lực: </span>
-              03/08/2022
-            </span>
-            <span className='nav-item'>
-              <span className='font-bold'>Cập nhật: </span>
-              03/10/2023
-            </span>
-          </nav>
-
-          <ul className='nav mb-2 flex items-center gap-5 justify-start text-sm text-light-gray'>
-            <li className='nav-item border bg-transparent border-light-gray rounded-md px-[8px] py-[4px] hover:bg-light-gray hover:text-white hover:border-white'>
-              <Link href='/resultVbl'>
-                <i class="bi bi-file-earmark-text"></i> Nội dung
-              </Link>
-            </li>
-            <li className='nav-item border bg-transparent border-light-gray rounded-md px-[8px] py-[4px] hover:bg-light-gray hover:text-white hover:border-white'>
-              <Link href='/resultVbl'>
-              <i class="bi bi-check2"></i> Thuộc tính
-              </Link>
-            </li>
-            <li className='nav-item border bg-transparent border-light-gray rounded-md px-[8px] py-[4px] hover:bg-light-gray hover:text-white hover:border-white'>
-              <Link href='/resultVbl'>
-              <i class="bi bi-list-task"></i> Văn bản liên quan
-              </Link>
-            </li>
-            <li className='nav-item border bg-transparent border-light-gray rounded-md px-[8px] py-[4px] hover:bg-light-gray hover:text-white hover:border-white'>
-              <Link href='/resultVbl'>
-              <i class="bi bi-diagram-2"></i> Lược đồ
-              </Link>
-            </li>
-            <li className='nav-item border border-light-gray rounded-md px-[8px] py-[4px] hover:bg-light-gray hover:text-white hover:border-white'>
-              <Link href='/resultVbl'>
-              <i class="bi bi-cloud-arrow-down"></i> Tải về
-              </Link>
-            </li>
-          </ul>
-      </div> */}
+      
       </main>
       <DropDown />
       <Pagination
